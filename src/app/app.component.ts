c31d492524b1235f9b03a9c4ecdd93d5223dd179
Lauren-D <laurent.dubois@uclouvain.be>
2019-04-29 16:29:34 +0200

    clean porject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,23 +18,4 @@ export class AppComponent {
     };
     firebase.initializeApp(config);
   }
-
-
-  postItems = [
-    {
-      title: 'Mon premier post',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et maximus nibh. Etiam posuere volutpat dui, eget dapibus erat tristique tincidunt. Maecenas aliquet scelerisque lacus, a commodo velit laoreet id',
-      loveIts: 0,
-    },
-    {
-      title: 'Mon deuxième post',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et maximus nibh. Etiam posuere volutpat dui, eget dapibus erat tristique tincidunt. Maecenas aliquet scelerisque lacus, a commodo velit laoreet id',
-      loveIts: 0,
-    },
-    {
-      title: 'Mon troisième post',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et maximus nibh. Etiam posuere volutpat dui, eget dapibus erat tristique tincidunt. Maecenas aliquet scelerisque lacus, a commodo velit laoreet id',
-      loveIts: 0,
-    }
-  ]
 }
